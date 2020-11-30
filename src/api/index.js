b8fa8a974b7f401513631bb09c8625b0f779fa70
mcmicko <mcmzrc@gmail.com>
2020-11-30 17:01:08 +0100

    complete country picker & chart

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,15 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) changeableUrl = `${url}/countries/${country}`;
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     const modifiedData = {
       // confirmed: data.confirmed
@@ -17,7 +21,9 @@ export const fetchData = async () => {
     };
 
     return modifiedData;
-  } catch (err) {}
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const fetchDailyData = async () => {
@@ -32,4 +38,14 @@ export const fetchDailyData = async () => {
 
     return modifiedData;
   } catch (err) {}
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {}
 };
